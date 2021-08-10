0e595c3cef47e2f2ead3f365cc68ad46eacd2966
Siddharth Vivek <gargantuan5k@outlook.com>
2021-08-10 12:50:06 +0530

    Add powershell script to setup new directory

--- git_commit.ps1
+++ git_commit.ps1
@@ -1,9 +1,13 @@
-param ([string]$m = "Commit to git")
+param ([string]$m="Commit to git")
 
+Write-Host "X---------------------------------------------------------------------X"
 Write-Host "Running git commit & push script"
+Write-Host "X---------------------------------------------------------------------X"
 
-git add .
-git commit -m $m
-git push origin master
+git add . | Write-Host
+git commit -m $m | Write-Host
+git push origin master | Write-Host
 
+Write-Host "X---------------------------------------------------------------------X"
 Write-Host "Done!"
+Write-Host "X---------------------------------------------------------------------X"
