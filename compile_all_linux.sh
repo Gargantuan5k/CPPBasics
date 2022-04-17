25d0be1fbf21e20baca464593045db52ac1b45ae
Siddharth Vivek <siddharth.vivek2000@outlook.com>
2022-04-17 12:34:58 +0530

    Add Shell Script to compile all C++ files (Linux)

---
+++ compile_all_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wd="$(readlink -f `dirname "$0"`)"
+
+for dir in $(readlink -f $(find $wd -maxdepth 1 -mindepth 1 -name "Section*")); do 
+    mkdir -p $dir/bin/linux
+    for file in $(find $dir -maxdepth 1 -mindepth 1 -name "*.cpp"); do
+        g++ $(readlink -f $file) -o "$dir/bin/linux/$(basename $file .cpp)"
+    done
+done
+echo "Compiled all .cpp files inside all Section directories (not "
